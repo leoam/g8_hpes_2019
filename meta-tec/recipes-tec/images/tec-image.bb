fb55a4fffdd0f49171afc25ceee9d42d0b6731f8
Leonardo Araya <leoam@hotmail.es>
2019-06-26 13:55:45 -0600

    Refactor repository distribution

--- meta-tec/recipes-tec/images/tec-image.bb
+++ meta-tec/recipes-tec/images/tec-image.bb
@@ -2,15 +2,24 @@ DESCRIPTION = "Simple helloworld application"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
-SRC_URI = "file:///home/leonardo/rpi/meta-tec/recipes-tec/myhello.c"
+SRC_URI = "file:///home/leonardo/rpi/meta-tec/recipes-tec/myhello.c \
+	   file:///home/leonardo/rpi/meta-tec/recipes-tec/intrinsics_results.pdf"
 
 S = "${WORKDIR}"
+FILES_${PN} += "/usr/share/tec/intrinsics_results.pdf"
 
 do_compile() {
 	${CC} /home/leonardo/rpi/meta-tec/recipes-tec/myhello.c ${LDFLAGS} -o myhello
 }
 
+# Install the converter code
 do_install() {
 	install -d ${D}${bindir}
 	install -m 0755 myhello ${D}${bindir}
+}
+
+# Install the documentation (PDF)
+do_install_append() {
+	install -d ${D}/usr/share/tec
+	install -m 0755 /home/leonardo/rpi/meta-tec/recipes-tec/intrinsics_results.pdf ${D}/usr/share/tec/intrinsics_results.pdf
 }
--- rgb2yuv_c.c
+++ rgb2yuv_c.c
@@ -43,14 +43,18 @@ int menu( int argc, char **argv, char **in_file, char **out_file){
 				printf("Authors of the program:\n\tLeonardo Araya\n\tGabriel Loria\n\tAlvaro Salazar\n");
 				break;
 			case 'h':
-				printf("Use '-i RGBfile' where RGBfile is the name of the file in RGB format to convert [REQUIRED].\nUse '-o YUVfile' where YUVfile is the name of the output file in YUV format [REQUIRED].\nUse '-a' to display information of the author of the program.\nUse '-h' to display this help guide.\n");
+				printf("Use '-i RGBfile' where RGBfile is the name of the file in RGB "
+				"format to convert [REQUIRED].\nUse '-o YUVfile' where YUVfile is the "
+				"name of the output file in YUV format [REQUIRED].\nUse '-a' to display "
+				"information of the author of the program.\nUse '-h' to display this "
+				"help guide.\n");
 				return 1;
 				break;
 			default:
 				printf("Not recognized option. Please use '-h' for help.\n");
 				return 1;
-		} //end block for switch
-	}  //end block for while
+		}
+	}
 	return 0;
 }
 
@@ -59,11 +63,7 @@ int pixel_rgb2yuv(struct RGB rgb, struct YUV *yuv){
 	yuv->Y = rgb.R * .299000 + rgb.G * .587000 + rgb.B * .114000;
 	yuv->U = rgb.R * -.168736 + rgb.G * -.331264 + rgb.B * .500000 + 128;
 	yuv->V = rgb.R * .500000 + rgb.G * -.418688 + rgb.B * -.081312 + 128;
-	/*
-	fprintf(stderr, "%ff\n", yuv->Y);
-	fprintf(stderr, "%ff\n", yuv->U);
-	fprintf(stderr, "%ff\n", yuv->V);
-	*/
+
 	return 0;
 }
 
@@ -78,24 +78,17 @@ int rgb2yuv(char *input_img_name, char *output_img_name){
 	struct YUV value;
 	int i = 0;
 	int status = 0;
-	printf("In function %s line %d \n",__FUNCTION__,__LINE__);
 
-	input.open(input_img_name, ios::in | ios::binary); //Example image input: "sample_640x480.rgb"
-	output.open(output_img_name, ios::out | ios::binary); //Example image output: "sample_639x480.yuv"
-  printf("In function %s line %d \n",__FUNCTION__,__LINE__);
+	input.open(input_img_name, ios::in | ios::binary);
+	output.open(output_img_name, ios::out | ios::binary);
 
 	input.read(input_read, sizeof(input_read));
 
-	printf("input_read %d \n",input_read[0]);
-	printf("input_read %d \n",input_read[1]);
-	printf("input_read %d \n",input_read[2]);
-	printf("In function %s line %d status %d \n",__FUNCTION__,__LINE__,status);
 	while (input.read(input_read, sizeof(input_read))) {
-		printf("In function %s line %d \n",__FUNCTION__,__LINE__);
 			// Convert the pixel from RGB to YUV
 			data.R = input_read[0];
-			data.R = input_read[1];
-			data.R = input_read[2];
+			data.G = input_read[1];
+			data.B = input_read[2];
 			status = pixel_rgb2yuv(data, &value);
 			if (status) {
 				fprintf(stderr, "status :%d\n", status);
@@ -105,42 +98,39 @@ int rgb2yuv(char *input_img_name, char *output_img_name){
 			converted_output[1]= (char)value.U;
 			converted_output[2]= (char)value.V;
 
-			printf(" i: %d",i);
-
-			if (i>12) {
-				printf("Y : %d\n", converted_output[0]);
-				printf("U : %d\n", converted_output[1]);
-				printf("V : %d\n", converted_output[2]);
-			}
-
-			memcpy(&output_pixels[i*3], converted_output, sizeof(converted_output));
-			i++;
+			memcpy(&output_pixels[i], converted_output, sizeof(converted_output));
+			i = i + 3;
 	}
-
-	printf("In function %s line %d \n",__FUNCTION__,__LINE__);
 
 	input.close();
 	output.write(output_pixels, sizeof(output_pixels));
 	output.close();
 
-
 	return 0;
 }
 
+// Main code starts here
 int main(int argc, char **argv){
-
 	char *RGB_file_name;
 	char *YUV_file_name;
-
 	int status = 0;
 
-	status = menu(argc,argv,&RGB_file_name,&YUV_file_name);
+	status = menu(argc,argv, &RGB_file_name, &YUV_file_name);
 	if (status) {
 		fprintf(stderr, "status :%d\n", status);
 		return status;
 	}
 
-	rgb2yuv(RGB_file_name,YUV_file_name);
+	// Start the time count
+	clock_t begin = clock();
+
+	// Start the convertion
+	rgb2yuv(RGB_file_name, YUV_file_name);
+
+	// Finish the time count
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Elapsed time: \t%fs\n", time_spent);
 
   return 0;
 }
